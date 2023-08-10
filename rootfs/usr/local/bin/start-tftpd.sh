0606248d451cfb40c2e23f5d82ba228fe970a966
casjay <git-admin@casjaysdev.com>
2023-08-10 19:57:32 -0400

    🗃️ Updated domain names 🗃️
    Dockerfile
    LICENSE.md
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/bin/start-tftpd.sh

--- rootfs/usr/local/bin/start-tftpd.sh
+++ rootfs/usr/local/bin/start-tftpd.sh
@@ -3,7 +3,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version           :  202211121554-git
 # @@Author           :  Jason Hempstead
-# @@Contact          :  jason@casjaysdev.com
+# @@Contact          :  jason@casjaysdev.pro
 # @@License          :  WTFPL
 # @@ReadME           :  start-tftpd.sh --help
 # @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
